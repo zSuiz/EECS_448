908397d39642e9c1066a029de10bacc84d6c8f99
Glenn P <47468320+zSuiz@users.noreply.github.com>
2019-10-01 10:09:07 -0500

    added files and fixed errors
    Background.css fixed + index errors fixed + password.js fnished

---
+++ Ex1/password.js
@@ -0,0 +1,22 @@
+ 
+
+//THIS JUST USE TO CHECK IF THERE ARE INPUT FROM THE BOXES
+document.addEventListener("DOMContentLoaded", ()=>{ 
+
+    let user = document.querySelector("#Input");
+    let user1 = document.querySelector("#validate");
+
+        user.addEventListener("input", (e) => 
+        {
+           // console.log("Password! ", user.value)
+           
+        })
+
+        user1.addEventListener("input", () => 
+        {
+           //console.log("Verify! ", user1.value)
+           
+        })
+       // document.querySelector("#passwordBox").innerText; 
+    
+    })
